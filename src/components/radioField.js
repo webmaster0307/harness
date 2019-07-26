84058eead457a94519e0ac0595c4fee4a9b5e10a
Kellen Mace <kellen.mace@gmail.com>
2019-07-26 14:52:31 -0400

    Select first radio button if no value has yet been set

--- src/components/radioField.js
+++ src/components/radioField.js
@@ -22,12 +22,21 @@ const RadioButtonLabel = styled.label`
 const RadioField = ({ formId, field, inputs, handleInputChange }) => {
   const { id, label, isRequired, choices } = field
 
+  const shouldButtonBeChecked = (index, value) => {
+    // If this is the first radio button and no value has been saved yet.
+    if (index === 0 && !inputs[id]) {
+      return true;
+    }
+
+    return inputs[id] === value
+  }
+
   return (
     <fieldset>
         <legend>{label}</legend>
         <RadioButtonsContainer>
           {choices.map(({text, value}, index) => (
-              <RadioButtonContainer key={value} checked={inputs[id] === value}>
+              <RadioButtonContainer key={value} checked={shouldButtonBeChecked(index, value)}>
                   <input
                       type="radio"
                       id={`choice_${formId}_${id}_${index}`}
@@ -36,7 +45,7 @@ const RadioField = ({ formId, field, inputs, handleInputChange }) => {
                       value={value}
                       onChange={event => handleInputChange(id, event)}
                       required={isRequired}
-                      checked={inputs[id] === value ? 'checked' : ''}
+                      checked={shouldButtonBeChecked(index, value) ? 'checked' : ''}
                   />
                   <RadioButtonLabel htmlFor={`choice_${formId}_${id}_${index}`}>{text}</RadioButtonLabel>
               </RadioButtonContainer>
