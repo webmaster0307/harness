833ab18900f3ef2ca1480f4da8c9384aa40735fe
Kellen Mace <kellen.mace@gmail.com>
2019-07-28 21:54:50 -0400

    Switch to a better hamburger SVG icon, resize Signature field to always conform to the width of its parent container

--- src/components/hamburgerIcon.js
+++ src/components/hamburgerIcon.js
@@ -1,16 +1,13 @@
 import React from "react";
 
 const HamburgerIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 25" style={{height:'100%'}}>
-    <path
-      fill="#CFCFCF"
-      fillRule="evenodd"
-      stroke="none"
-      strokeWidth="1"
-      d="M4 8.421h26v4.21H4v-4.21zm0 10.526h26v4.21H4v-4.21zm0 9.474h26v4.21H4v-4.21z"
-      transform="translate(-669 -107) translate(30 89) translate(635 10)"
-    />
-    </svg>
+  <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1024 1024"
+      style={{height:'100%'}}
+    >
+      <path fill="#CFCFCF" d="M57.556 740.373l-.18 145.8H304.49v-145.8H57.556zm242.35 0v145.8H956.17l-.045-145.8H299.907zM57.557 448.776l-.18 145.799H304.49V448.776H57.556zm242.35 0v145.799H956.17l-.045-145.799H299.907zm-242.35-291.62l-.18 145.822H304.49V157.156H57.556zm898.591-.135l-656.24.135v145.822H956.17l-.023-145.957z" />
+  </svg>
 );
 
 export default HamburgerIcon
--- src/components/signatureField.js
+++ src/components/signatureField.js
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import styled from 'styled-components'
 import SignatureCanvas from 'react-signature-canvas'
+import { debounce } from 'lodash'
 
 const SignatureFieldContainer = styled.div`
   .signature-canvas {
     background: ${props => props.theme.white};
     border: 1px solid #bebebe;
-    border-radius: ${props => props.theme.borderRadius}
+    border-radius: ${props => props.theme.borderRadius};
   }
 `
 
 const SignatureField = ({ formId, field, inputs, handleInputChange }) => {
   const { id, label, isRequired } = field
+  const canvasEl = useRef(null);
+  const canvasParentEl = useRef(null);
   const htmlId = `input_${formId}_${id}`;
 
+  // Set canvas width to match its parent's width.
+  const handleResize = () => {
+    const parentWidth = canvasParentEl.current.offsetWidth
+    canvasEl.current.getCanvas().setAttribute("width", parentWidth)
+  }
+
+  useEffect(() => {
+    // Set canvas width once on initial render.
+    handleResize()
+
+    window.addEventListener('resize', debounce(handleResize, 200));
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  })
+
   return (
-    <SignatureFieldContainer>
+    <SignatureFieldContainer ref={canvasParentEl}>
       <p>{label}</p>
       <SignatureCanvas
+        ref={canvasEl}
         penColor='black'
-        canvasProps={{width: 407, height: 200, className: 'signature-canvas'}}
+        canvasProps={{height: 200, className: 'signature-canvas'}}
       />
     </SignatureFieldContainer>
   )
